1392a7f7a7622be521519f1fcb55403be7f9b58d
sharmaine1028 <sharmaine1028@gmail.com>
2026-01-23 00:13:36 +0800

    Update vite.config.ts to correct base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/will-you-be-my-valentine",
   plugins: [react(), tailwindcss()],
 });
